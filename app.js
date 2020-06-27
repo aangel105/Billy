d84d935954c4efe4c23df502504f2fe3e1a12060
angel arias <angelarias105@gmail>
2020-06-26 21:59:35 -0400

    working on the hamburger menu and media queries to website

--- app.js
+++ app.js
@@ -1,19 +1,33 @@
-const btnOne = document.querySelector('.btn-one');
-const modal = document.querySelector('#modal');
-const closeModal = document.querySelector('.close');
+const btnOne = document.querySelector(".btn-one");
+const modal = document.querySelector("#modal");
+const closeModal = document.querySelector(".close");
+const menu = document.querySelector(".menu");
+const navLinks = document.querySelector(".nav-links");
+const menuBg = document.querySelector(".menu-bg");
 
-btnOne.addEventListener("click", function () {
+btnOne.addEventListener("click", () => {
   modal.style.display = "block";
 });
 
-closeModal.addEventListener("click", function () {
+closeModal.addEventListener("click", () => {
     modal.style.display = "none";
 });
 
 // close modal clicking anywhere outside the modal
 
-window.addEventListener("click", function (e) {
+window.addEventListener("click", (e) => {
   if (e.target === modal) {
     modal.style.display = "none";
   }
 });
+
+// Hamburger menu
+
+menu.addEventListener('click', () => {
+  menu.classList.toggle("change");
+  navLinks.classList.toggle("change");
+  menuBg.classList.toggle("change-bg");
+});
+
+
+
