460f679bbfca45c0cc1d00de294ef32ebf1583ff
angel arias <angelarias105@gmail>
2020-07-02 18:44:25 -0400

    made the landing page responsives for all devices

--- app.js
+++ app.js
@@ -3,7 +3,18 @@ const modal = document.querySelector("#modal");
 const closeModal = document.querySelector(".close");
 const menu = document.querySelector(".menu");
 const navLinks = document.querySelector(".nav-links");
+const links = document.querySelector("li a")
 const menuBg = document.querySelector(".menu-bg");
+
+// Hamburger menu
+
+menu.addEventListener("click", () => {
+  console.log('btn-clicked');
+  menu.classList.toggle("change");
+  navLinks.classList.toggle("change");
+  links.classList.toggle("current");
+  menuBg.classList.toggle("change-bg");
+});
 
 btnOne.addEventListener("click", () => {
   modal.style.display = "block";
@@ -13,18 +24,10 @@ closeModal.addEventListener("click", () => {
   modal.style.display = "none";
 });
 
-// close modal clicking anywhere outside the modal
+// // close modal clicking anywhere outside the modal
 
 window.addEventListener("click", (e) => {
   if (e.target === modal) {
     modal.style.display = "none";
   }
-});
-
-// Hamburger menu
-
-menu.addEventListener("click", () => {
-  menu.classList.toggle("change");
-  navLinks.classList.toggle("change");
-  menuBg.classList.toggle("change-bg");
 });
